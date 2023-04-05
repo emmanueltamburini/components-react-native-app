818727e4f3b26c9ea50d3da84c3ccc37fa03a589
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-04-05 08:58:55 -0400

    use animation

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,53 +1,29 @@
-import React, {useRef} from 'react';
-import {
-  View,
-  StyleSheet,
-  Animated,
-  ViewStyle,
-  Button,
-  Easing,
-} from 'react-native';
+import React from 'react';
+import {View, StyleSheet, Animated, ViewStyle, Button} from 'react-native';
+import {useAnimation} from '../hooks/useAnimation';
 
 export const Animation101Screen = () => {
-  const opacity = useRef(new Animated.Value(0)).current;
-  const top = useRef(new Animated.Value(-100)).current;
+  const {opacity, position, fadeIn, fadeOut, startMoving, goToPosition} = useAnimation();
 
-  const fadeIn = () => {
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start(() => console.log('FadeIn animation finished'));
-
-    Animated.timing(top, {
-      toValue: 0,
-      duration: 1000,
-      useNativeDriver: true,
-      easing: Easing.bounce,
-    }).start(() => console.log('top animation finished'));
-  };
-
-  const fadeOut = () => {
-    Animated.timing(opacity, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start(() => console.log('FadeOut animation finished'));
-
-    Animated.timing(top, {
-      toValue: -100,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start(() => console.log('top animation finished'));
-  };
-
-  const styles = stylesFunction(opacity, top);
+  const styles = stylesFunction(opacity, position);
 
   return (
     <View style={styles.container}>
       <Animated.View style={styles.purpleBox} />
-      <Button title="Fade in" onPress={fadeIn} />
-      <Button title="Fade out" onPress={fadeOut} />
+      <Button
+        title="Fade in"
+        onPress={() => {
+          fadeIn();
+          startMoving(-100, 1000);
+        }}
+      />
+      <Button
+        title="Fade out"
+        onPress={() => {
+          fadeOut();
+          goToPosition(-100, 1000);
+        }}
+      />
     </View>
   );
 };
