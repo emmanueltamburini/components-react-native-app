7a171e11b6d2ad93f8b017365a43ae181a6db610
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-04-06 10:52:59 -0400

    Keyboard avoidning, ScrollView and TouchableWithoutFeedback view have added

--- src/screens/TextInputs.tsx
+++ src/screens/TextInputs.tsx
@@ -1,5 +1,13 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 
@@ -18,32 +26,39 @@ export const TextInputs = () => {
   };
 
   return (
-    <View style={currentStyles.container}>
-      <HeaderTitle title="TextInputs" />
-      <TextInput
-        style={currentStyles.textInput}
-        placeholder="Enter your name"
-        autoCorrect={false}
-        autoCapitalize="words"
-        onChangeText={value => onChange(value, 'name')}
-      />
-      <TextInput
-        style={currentStyles.textInput}
-        placeholder="Enter your email"
-        autoCorrect={false}
-        autoCapitalize="none"
-        onChangeText={value => onChange(value, 'email')}
-        keyboardType="email-address"
-        keyboardAppearance="dark"
-      />
-      <TextInput
-        style={currentStyles.textInput}
-        placeholder="Enter your phone"
-        onChangeText={value => onChange(value, 'phone')}
-        keyboardType="phone-pad"
-      />
-      <HeaderTitle title={JSON.stringify(form, null, 3)} />
-    </View>
+    <KeyboardAvoidingView>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={currentStyles.container}>
+            <HeaderTitle title="TextInputs" />
+            <TextInput
+              style={currentStyles.textInput}
+              placeholder="Enter your name"
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={value => onChange(value, 'name')}
+            />
+            <TextInput
+              style={currentStyles.textInput}
+              placeholder="Enter your email"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={value => onChange(value, 'email')}
+              keyboardType="email-address"
+              keyboardAppearance="dark"
+            />
+            <TextInput
+              style={currentStyles.textInput}
+              placeholder="Enter your phone"
+              onChangeText={value => onChange(value, 'phone')}
+              keyboardType="phone-pad"
+            />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <View style={currentStyles.spacer} />
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -59,5 +74,8 @@ const currentStyles = StyleSheet.create({
     paddingHorizontal: 10,
     borderRadius: 10,
     marginVertical: 10,
+  },
+  spacer: {
+    height: 100,
   },
 });
