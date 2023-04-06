1dbdded637b4dce0098535fc371cf6e82a495391
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-04-05 20:45:02 -0400

    Custom switch has added

--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,21 +1,47 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Switch, Platform} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
+import {CustomSwitch} from '../components/CustomSwitch';
 
 export const SwitchScreen = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
+  const onChange = (value: boolean, field: keyof typeof state) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+  };
 
   return (
     <View style={styles.container}>
       <HeaderTitle title="Switches" />
-      <Switch
-        trackColor={{false: '#D9D9DB', true: '#5856D6'}}
-        thumbColor={Platform.OS === 'android' ? '#5856D6' : ''}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isActive</Text>
+        <CustomSwitch
+          onChange={value => onChange(value, 'isActive')}
+          isOn={state.isActive}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHungry</Text>
+        <CustomSwitch
+          onChange={value => onChange(value, 'isHungry')}
+          isOn={state.isHungry}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHappy</Text>
+        <CustomSwitch
+          onChange={value => onChange(value, 'isHappy')}
+          isOn={state.isHappy}
+        />
+      </View>
+      <Text style={styles.switchText}>{JSON.stringify(state, null, 3)}</Text>
     </View>
   );
 };
@@ -24,5 +50,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginHorizontal: 20,
+  },
+  switchText: {
+    fontSize: 25,
+    marginVertical: 10,
+  },
+  switchRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
