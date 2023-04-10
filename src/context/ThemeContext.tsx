696b446f6c14dd1d5d38d6f101fccf75483b4d4c
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-04-09 20:27:37 -0400

    app has finished

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,7 +1,6 @@
 import React, {createContext, useEffect, useMemo, useReducer} from 'react';
 import {ThemeState, darkTheme, lightTheme, themeReducer} from './themeReducer';
-import {Appearance, useColorScheme} from 'react-native';
-import {AppState} from 'react-native';
+import {useColorScheme} from 'react-native';
 
 interface Props {
   children: JSX.Element | JSX.Element[];
@@ -31,14 +30,8 @@ export const ThemeProvider = ({children}: Props) => {
   );
 
   useEffect(() => {
-    AppState.addEventListener('change', status => {
-      if (status === 'active') {
-        Appearance.getColorScheme() === 'light'
-          ? setLightTheme()
-          : setDarkTheme();
-      }
-    });
-  }, []);
+    colorScheme === 'light' ? setLightTheme() : setDarkTheme();
+  }, [colorScheme]);
 
   const setDarkTheme = () => dispatch({type: 'SET_DARK_THEME'});
   const setLightTheme = () => dispatch({type: 'SET_LIGHT_THEME'});
